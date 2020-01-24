80e2548d328254a7c3259ff8d2d7e0ab1108697b
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-24 18:43:18 -0300

    clean comments

--- implementation/kangaroo.js
+++ implementation/kangaroo.js
@@ -1,10 +1,10 @@
 // Complete the kangaroo function below.
 function kangaroo(x1, v1, x2, v2) {
     let result = 'NO'
-
-    for(let i = 0; i <= 10000; i++) {
-        if(x1 + v1 * i === x2 + v2 * i)
-        result = 'YES'
+    for (let i = 0; i <= 10000; i++) {
+        if (x1 + v1 * i === x2 + v2 * i) {
+            result = 'YES'
+        }
     }
     return result;
 }
@@ -21,13 +21,13 @@ function kangaroo(x1, v1, x2, v2) {
 // const x2 = 5
 // const v2 = 3
 
-// YES 2081 8403 9107 8400
+// YES
 // const x1 = 2081
 // const v1 = 8403
 // const x2 = 9107
 // const v2 = 8400
 
-// NO 21 6 47 3
+// NO
 const x1 = 21
 const v1 = 6
 const x2 = 47
