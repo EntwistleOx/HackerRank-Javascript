accda32775f83665005f772d30e296188322e09d
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-29 15:01:44 -0300

    breaking the records

---
+++ implementation/breakingTheRecords.js
@@ -0,0 +1,9 @@
+// Complete the breakingRecords function below.
+function breakingRecords(scores) {
+
+}
+
+const scores = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
+const result = breakingRecords(scores);
+
+console.log(result.join(' ') + '\n');
