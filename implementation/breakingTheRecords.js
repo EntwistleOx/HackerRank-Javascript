85394af00d9aeb2e0bbd1ced8959836827cb0282
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-30 18:33:06 -0300

    begin

--- implementation/breakingTheRecords.js
+++ implementation/breakingTheRecords.js
@@ -1,6 +1,20 @@
 // Complete the breakingRecords function below.
 function breakingRecords(scores) {
 
+    let countH = 0;
+    let countL = 0;
+
+    for (let i = 0; i < scores.length; i++) {
+
+        for (let j = 1; j < scores.length; j++) {
+
+            console.log(scores[j], scores[i])
+
+        }
+
+    }
+
+    return [countH, countL]
 }
 
 const scores = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
